c9aa970e227d4e5c22b6665f97a17f923322e6dd
Liu Xiaoyi <circuitcoder0@gmail.com>
2018-11-11 06:56:24 +0800

    Added: pong route

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <memory>
+#include <string>
 
 #include <crow.h>
 
 using namespace std;
 
+unique_ptr<crow::App<>> app;
+
+void init_server();
+void join_server();
+
 int main() {
-  cout<<"Started"<<endl;
+  init_server();
+  cout<<"Start..."<<endl;
+  join_server();
   return 0;
+}
+
+void init_server() {
+  app = unique_ptr<crow::App<>>(new crow::App<>());
+
+  crow::App<> &handle = *app;
+
+  auto pong = [](string name) -> string {
+    return string("Hello, ") + name;
+  };
+
+  CROW_ROUTE(handle, "/hello/<string>").methods("GET"_method)(pong);
+}
+
+void join_server() {
+  app->port(6473).run();
 }
